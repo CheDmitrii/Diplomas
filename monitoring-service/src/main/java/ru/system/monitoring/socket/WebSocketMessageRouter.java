0063a0d61b5560b8910249f4889a0e9b87314e1b
CheDmitrii <dima.chebykin2000@mail.ru>
2025-05-31 12:55:25 +0300

    chang socket to frontend work

--- monitoring-service/src/main/java/ru/system/monitoring/socket/WebSocketMessageRouter.java
+++ monitoring-service/src/main/java/ru/system/monitoring/socket/WebSocketMessageRouter.java
@@ -3,12 +3,18 @@ package ru.system.monitoring.socket;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.CloseStatus;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 import ru.system.monitoring.socket.handler.SocketMessageHandler;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -17,10 +23,12 @@ import java.util.stream.Collectors;
 @Component
 @Slf4j
 public class WebSocketMessageRouter implements WebSocketHandler {
+    private final ReactiveJwtDecoder jwtDecoder;
     private final Map<String, SocketMessageHandler> handlers;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public WebSocketMessageRouter(List<SocketMessageHandler> handlers) {
+    public WebSocketMessageRouter(List<SocketMessageHandler> handlers, ReactiveJwtDecoder jwtDecoder) {
+        this.jwtDecoder = jwtDecoder;
         this.handlers = handlers.stream()
                 .collect(Collectors.toMap(SocketMessageHandler::getType, Function.identity()));
     }
@@ -28,28 +36,45 @@ public class WebSocketMessageRouter implements WebSocketHandler {
 
     @Override
     public Mono<Void> handle(WebSocketSession session) {
-        return session.receive()
-                .map(webSocketMessage -> webSocketMessage.getPayloadAsText())
-                .flatMap(payload -> {
-                    try {
-                        log.info("--------- socket message received ---------");
-                        log.info(payload);
-                        JsonNode jsonNode = objectMapper.readTree(payload);
-                        String key = jsonNode.has("action") ? jsonNode.get("action").asText()
-                                : jsonNode.has("type") ? jsonNode.get("type").asText()
-                                : null;
-                        if (key == null) {
-                            // Неизвестный тип сообщения
-                            return Mono.empty();
-                        }
-                        SocketMessageHandler handler = handlers.get(key);
-                        return handler.handle(jsonNode, session);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        return Mono.empty();
-                    }
-                })
-                .then();
+        URI uri = session.getHandshakeInfo().getUri();
+        String token = UriComponentsBuilder.fromUri(uri)
+                .build()
+                .getQueryParams()
+                .getFirst("token");
+
+        if (token == null) {
+            return session.close(CloseStatus.NOT_ACCEPTABLE.withReason("Missing token"));
+        }
+        return this.validateJwtToken(token)
+                .flatMap(jwt ->
+                    session.receive()
+                            .map(WebSocketMessage::getPayloadAsText)
+                            .flatMap(payload -> {
+                                try {
+                                    log.info("--------- socket message received ---------");
+                                    log.info(payload);
+                                    JsonNode jsonNode = objectMapper.readTree(payload);
+                                    String key = jsonNode.has("action") ? jsonNode.get("action").asText()
+                                            : jsonNode.has("type") ? jsonNode.get("type").asText()
+                                            : null;
+                                    if (key == null) {
+                                        // Неизвестный тип сообщения
+                                        return Mono.empty();
+                                    }
+                                    SocketMessageHandler handler = handlers.get(key);
+                                    return handler.handle(jsonNode, session);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                    return Mono.empty();
+                                }
+                            })
+                            .then()
+                )
+                .onErrorResume(e -> session.close(CloseStatus.NOT_ACCEPTABLE.withReason("Invallid token")));
+    }
+
+    public Mono<Jwt> validateJwtToken(String token) {
+        return jwtDecoder.decode(token);
     }
 }
 
