e21f16c9ffb4247a2723eb6f5e2a7fa682431323
CheDmitrii <dima.chebykin2000@mail.ru>
2025-05-31 16:52:09 +0300

    change socket then frontend connection work

--- monitoring-service/src/main/java/ru/system/monitoring/config/security/SecurityConfig.java
+++ monitoring-service/src/main/java/ru/system/monitoring/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.system.monitoring.config.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -26,6 +27,7 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.GET, "/socket").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> jwt.jwkSetUri(jwkSetUri)));
