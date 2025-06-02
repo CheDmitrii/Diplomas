459037cf9c4ca24b9953c9b7b15524d61a2fd71c
CheDmitrii <dima.chebykin2000@mail.ru>
2025-06-02 14:22:40 +0300

    change admin controller logic

---
+++ authentication-service/src/main/java/ru/system/authentication/service/ClaimService.java
@@ -0,0 +1,46 @@
+package ru.system.authentication.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import ru.system.library.exception.HttpResponseEntityException;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+public class ClaimService {
+    public UUID getUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication.getCredentials() == null || !(authentication.getCredentials() instanceof Jwt jwt)) {
+            throw new HttpResponseEntityException(HttpStatus.UNAUTHORIZED, "User is not authenticated");
+        }
+        String userId = jwt.getClaimAsString("user_id");
+        if (userId == null) {
+            throw new HttpResponseEntityException(HttpStatus.BAD_REQUEST, "Bad jwt, wrong user_id");
+        }
+        return UUID.fromString(userId);
+    }
+
+    public String getRole() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication.getCredentials() == null || !(authentication.getCredentials() instanceof Jwt jwt)) {
+            throw new HttpResponseEntityException(HttpStatus.UNAUTHORIZED, "User is not authenticated");
+        }
+        Object claim = jwt.getClaim("role");
+        if (!(claim instanceof Map)) {
+            throw new HttpResponseEntityException(HttpStatus.BAD_REQUEST, "Bad jwt, user doesn't have role");
+        }
+        Object value = ((Map<String, Object>) claim).get("value");
+        if (value == null) {
+            throw new HttpResponseEntityException(HttpStatus.BAD_REQUEST, "Bad jwt, user doesn't have role");
+        }
+        return value.toString();
+    }
+
+    public boolean hasFullPermission() {
+        return this.getRole().equalsIgnoreCase("admin");
+    }
+}
